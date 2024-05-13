01a069d2f094ab6a891b62ee740eacfb80c7e6f2
quy.le <quy.le@bizzi.vn>
2024-05-13 09:59:05 +0700

    update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://quylk2906.github.io/garbage-demo',
   plugins: [react()],
-})
+});
